b22ec0f6ed2b88a9445ca99e461f5380e224cba3
Enyus <enyus@hotmail.com>
2022-09-15 12:15:15 -0300

    Adicionando até o minuto 44 da aula 3 (ignite)

---
+++ server/README.md
@@ -0,0 +1,34 @@
+# Backend
+
+## Entidades
+
+### Games
+id
+title
+bannerUrl
+
+CDN (Content Delivery Network) (Amazon S3)
+
+### Ads
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+1:30h -> 90m
+R$ 179,99 -> 17999
+Datas (fuso horário / formatos diferentes i18n)
+Pontos Flutuantes
+
+## Casos de Uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game (mobile)
+- Buscar discord pelo id do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,56 @@
 // const express = require('express');
 
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
+// HTTP Methods / API RESTful / HTTP Codes
+// GET, POST, PUT, DELETE, PATCH
+/* Tipos de Parâmentros:
+    Query: /ads?page=2&sort=title
+        -> URL
+        -> Persistir estado (filtros, orgenação, paginação, nãosensíveis)
+        -> nomeados
+        -> request.query
+    Route: /ads/5
+        -> identificação de um recurso
+        -> :id -> Informação dinâmica
+        -> request.params
+    Body:
+        -> formulários
+        -> enviar várias informações ao mesmo tempo
+        -> request.body
+*/
+
+// Listagem de games com contagem de anúncios:
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+// Criar anúnico de um game:
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
 // www.minhaapi.com/ads
 
-app.get ('/ads', (request, response)=> {
+// Listagem de Anúnicos por Game 
+app.get ('/games/:id/ads', (request, response)=> {
+    // const gameId = request.params.id;
+
     return response.json([
         {id:1, name:'Anúncio 1'},
         {id:2, name:'Anúncio 2'},
         {id:3, name:'Anúncio 3'},
         {id:4, name:'Anúncio 4'}
     ])
+});
+
+// Buscar discord pelo id do anúncio:
+app.get ('/ads/:id/discord', (request, response)=> {
+    // const adId = request.params.id;
+
+    return response.json([])
 });
 
 app.listen(3333);
